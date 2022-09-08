e31c109c7aa1e6151cf2515b6020b26af194d4dd
Arkhin <nabillouh62110@gmail.com>
2022-09-08 16:54:11 +0200

    creation formulaire de creation d'utilisateur

--- src/Controller/UtilisateurControlleur.php
+++ src/Controller/UtilisateurControlleur.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
 use App\Repository\CommentaireRepository;
 use App\Repository\PublicationRepository;
 use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UtilisateurControlleur extends AbstractController
@@ -22,7 +25,28 @@ class UtilisateurControlleur extends AbstractController
         ]);
     }
 
-    #[Route(path:'/utilisteur/{name}', name: 'app_utilisateur_show')]
+    #[Route('/utilisateur/create', name: 'app_utilisateur_create')]
+    public function create(Request $request, ManagerRegistry $doctrine, EntityManagerInterface $manager)
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($utilisateur);
+            $manager->flush();
+
+            $this->addFlash('success', $utilisateur->getName().'a été créé');
+
+            return $this->redirectToRoute('app_utilisateur_index');
+        }
+        return $this->render('utilisateur/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route(path:'/utilisateur/{name}', name: 'app_utilisateur_show')]
     public function show(UtilisateurRepository $repository, Utilisateur $utilisateur, CommentaireRepository $repositorycom, PublicationRepository $repositorypubli)
     {
         $commentaires = $repositorycom->findAll();
