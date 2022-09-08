621820b887ac0781f55200fa74f808ac0991cee4
Arkhin <nabillouh62110@gmail.com>
2022-09-08 11:04:32 +0200

    creation page d'accueil

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelcomeController extends AbstractController
+{
+    #[Route(path: '/', name: 'home')]
+    public function index()
+    {
+        return $this->render('welcome/index.html.twig');
+    }
+}
